b4623c4215055627d5a6cf73f0bffd04951877b9
dooyeoung <dooyeoung.ryu@gmail.com>
2022-08-10 15:00:37 +0900

    레포지토리 수정

--- app/repository/url.py
+++ app/repository/url.py
@@ -1,21 +1,30 @@
 import abc
-from typing import Optional
+from re import T
+from typing import Optional, Sequence
 
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound
 
 from app.models.url import Url
+from app.exc import NotFoundUrl
 
 class UrlRepository(abc.ABC):
     @abc.abstractmethod
     def get_url_by_source_url(
-        self, session: Session, **path_parameters
+        self, session: Session, **parameters
     ) -> Optional[Url]:
         pass
 
-    
+
     def get_url_by_short_id(
-        self, session: Session, **path_parameters
-    ) -> Optional[Url]:
+        self, session: Session, **parameters
+    ) -> Url:
+        pass
+
+
+    def get_all(
+        self, session: Session, **parameters
+    ) -> Sequence[Url]:
         pass
 
 
@@ -29,7 +38,7 @@ class SQLAlchemyUrlRepository(UrlRepository):
             Url
         ).filter(
             Url.source_url == source_url
-        ).one_or_none()
+        ).first()
 
 
     def get_url_by_short_id(
@@ -37,8 +46,22 @@ class SQLAlchemyUrlRepository(UrlRepository):
         session: Session,
         short_id: str,
     ) -> Optional[Url]:
+        datas = None
+        try:
+            datas = session.query(
+                Url
+            ).filter(
+                Url.short_id == short_id
+            ).one()
+        except NoResultFound:
+            raise NotFoundUrl
+        return datas
+
+
+    def get_all(
+        self,
+        session: Session,
+    ) -> Sequence[Url]:
         return session.query(
             Url
-        ).filter(
-            Url.short_id == short_id
-        ).one_or_none()
+        ).all()
